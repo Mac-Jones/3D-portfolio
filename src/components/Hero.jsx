f18f25997d12412458783beebcd3bd830061c1d4
macjonessoriano <sorianomacjones@gmail.com>
2023-09-19 17:40:37 +0800

    modify styles

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,8 +3,8 @@ import { ComputersCanvas } from './canvas';
 
 const Hero = () => {
 	return (
-		<section className='relative w-full h-screen mx-auto z-0'>
-			<div className='padding-x absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5 '>
+		<section className='relative w-full h-screen mx-auto z-5'>
+			<div className='padding-x absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5'>
 				<div className='flex flex-col justify-center items-center mt-5'>
 					<div className='w-5 h-5 rounded-full bg-[#915eff]' />
 					<div className='w-1 sm:h-80 h-40 violet-gradient' />
@@ -23,7 +23,9 @@ const Hero = () => {
 				</div>
 			</div>
 
-			<ComputersCanvas />
+			<div className='z-10 w-full'>
+				<ComputersCanvas />
+			</div>
 
 			<div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
 				<a href='#about'>
