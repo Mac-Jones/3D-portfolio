2ce0968922d665d62a427a233b792fb95b791ee2
macjonessoriano <sorianomacjones@gmail.com>
2023-09-19 18:00:55 +0800

    change something

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import { ComputersCanvas } from './canvas';
 
 const Hero = () => {
 	return (
-		<section className='relative w-full h-screen mx-auto z-5'>
+		<section className='relative w-full h-screen mx-auto'>
 			<div className='padding-x absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5'>
 				<div className='flex flex-col justify-center items-center mt-5'>
 					<div className='w-5 h-5 rounded-full bg-[#915eff]' />
@@ -23,9 +23,12 @@ const Hero = () => {
 				</div>
 			</div>
 
-			<div className='z-10 w-full'>
-				<ComputersCanvas />
-			</div>
+			<motion.div
+				// variants={slideIn('right', 'tween', 0.2, 1)}
+				className='xl:flex-1 xl:h-auto  h-screen mt-0'
+			>
+				<ComputersCanvas />{' '}
+			</motion.div>
 
 			<div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
 				<a href='#about'>
