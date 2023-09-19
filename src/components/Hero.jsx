419f2a6a1390a8cdefd86c029534333dc3e26a4e
macjonessoriano <sorianomacjones@gmail.com>
2023-09-19 17:27:00 +0800

    fourth commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import { ComputersCanvas } from './canvas';
 
 const Hero = () => {
 	return (
-		<section className='relative w-full h-screen mx-auto'>
+		<section className='relative w-full h-screen mx-auto z-0'>
 			<div className='padding-x absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5 '>
 				<div className='flex flex-col justify-center items-center mt-5'>
 					<div className='w-5 h-5 rounded-full bg-[#915eff]' />
