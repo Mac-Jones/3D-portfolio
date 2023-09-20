0623d2ac53aff1df8dafe1df09f43f88c729b552
macjonessoriano <sorianomacjones@gmail.com>
2023-09-20 15:49:43 +0800

    commit

--- src/components/canvas/Computer.jsx
+++ src/components/canvas/Computer.jsx
@@ -26,3 +26,44 @@ const Computer = () => {
 };
 
 export default Computer;
+
+// import { Suspense, useEffect, useState } from 'react';
+// import { Canvas } from '@react-three/fiber';
+// import { OrbitControls, Preload } from '@react-three/drei';
+
+// import { useMediaQuery } from 'react-responsive';
+
+// import CanvasLoader from '../Loader';
+// import Computer from './Computer';
+
+// const ComputerCanvas = () => {
+// 	const [mobileView, setMobileView] = useState(false);
+
+// 	const isMobile = useMediaQuery({ query: '(max-width: 500px)' });
+// 	useEffect(() => {
+// 		setMobileView(isMobile);
+// 	}, [isMobile]);
+
+// 	return (
+// 		<Canvas
+// 			frameloop='demand'
+// 			shadows
+// 			dpr={[1, 2]}
+// 			camera={{ position: [20, 3, 5], fov: 25 }}
+// 			gl={{ preserveDrawingBuffer: true }}
+// 		>
+// 			<Suspense fallback={<CanvasLoader />}>
+// 				<OrbitControls
+// 					enableZoom={false}
+// 					maxPolarAngle={Math.PI / 2}
+// 					minPolarAngle={Math.PI / 2}
+// 				/>
+// 				<Computer isMobile={mobileView} />
+// 			</Suspense>
+
+// 			<Preload all />
+// 		</Canvas>
+// 	);
+// };
+
+// export default ComputerCanvas;
