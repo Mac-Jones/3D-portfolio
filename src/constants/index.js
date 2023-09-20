bfbabed22e2cfd9f9a37f41c152dc1267d6b4842
macjonessoriano <sorianomacjones@gmail.com>
2023-09-20 18:50:48 +0800

    commit

--- src/constants/index.js
+++ src/constants/index.js
@@ -59,6 +59,26 @@ const services = [
 
 const technologies = [
 	{
+		name: 'CSS 3',
+		icon: css,
+	},
+	{
+		name: 'JavaScript',
+		icon: javascript,
+	},
+	{
+		name: 'TypeScript',
+		icon: typescript,
+	},
+	{
+		name: 'React JS',
+		icon: reactjs,
+	},
+	{
+		name: 'Redux Toolkit',
+		icon: redux,
+	},
+	{
 		name: 'Tailwind CSS',
 		icon: tailwind,
 	},
@@ -84,26 +104,6 @@ const technologies = [
 	{
 		name: 'HTML 5',
 		icon: html,
-	},
-	{
-		name: 'CSS 3',
-		icon: css,
-	},
-	{
-		name: 'JavaScript',
-		icon: javascript,
-	},
-	{
-		name: 'TypeScript',
-		icon: typescript,
-	},
-	{
-		name: 'React JS',
-		icon: reactjs,
-	},
-	{
-		name: 'Redux Toolkit',
-		icon: redux,
 	},
 ];
 
