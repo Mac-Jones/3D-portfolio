75b7d9cae6ea8bb78af970e36d879671fdf0b9d6
macjonessoriano <sorianomacjones@gmail.com>
2023-09-20 18:34:20 +0800

    modify computer

--- src/components/canvas/ComputerCanvas.jsx
+++ src/components/canvas/ComputerCanvas.jsx
@@ -16,24 +16,20 @@ const ComputerCanvas = () => {
 	// }, [isMobile]);
 
 	return (
-		<Canvas
-			frameloop='demand'
-			shadows
-			dpr={[1, 2]}
-			camera={{ position: [20, 3, 5], fov: 25 }}
-			gl={{ preserveDrawingBuffer: true }}
-		>
-			<Suspense fallback={<CanvasLoader />}>
-				<OrbitControls
-					enableZoom={false}
-					maxPolarAngle={Math.PI / 2}
-					minPolarAngle={Math.PI / 2}
-				/>
-				<Computer />
-			</Suspense>
+		<div className='absolute inset-0'>
+			<Canvas camera={{ position: [20, 3, 5], fov: 25 }}>
+				<Suspense fallback={<CanvasLoader />}>
+					<OrbitControls
+						enableZoom={false}
+						maxPolarAngle={Math.PI / 2}
+						minPolarAngle={Math.PI / 2}
+					/>
+					<Computer />
+				</Suspense>
 
-			<Preload all />
-		</Canvas>
+				<Preload all />
+			</Canvas>
+		</div>
 	);
 };
 
