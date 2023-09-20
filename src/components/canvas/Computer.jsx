4298cfb0e2c995e70154a737a3e39c546a1b30ed
macjonessoriano <sorianomacjones@gmail.com>
2023-09-20 18:09:17 +0800

    haha

--- src/components/canvas/Computer.jsx
+++ src/components/canvas/Computer.jsx
@@ -5,7 +5,7 @@ const Computer = () => {
 
 	return (
 		<mesh>
-			<hemisphereLight intensity={0.15} groundColor='black' />
+			<hemisphereLight intensity={1} groundColor='black' />
 			<spotLight
 				position={[-20, 50, 10]}
 				angle={0.12}
