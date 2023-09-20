085b0170bd2301d90a69f47ae91a658ea4d959c7
macjonessoriano <sorianomacjones@gmail.com>
2023-09-20 15:22:19 +0800

    modified

--- src/components/canvas/Computer.jsx
+++ src/components/canvas/Computer.jsx
@@ -19,12 +19,21 @@ const Computer = () => {
 				shadow-mapSize={1024}
 			/>
 			<pointLight intensity={1} />
-			<primitive
-				object={computer.scene}
-				scale={isMobile ? 0.7 : 0.75}
-				position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
-				rotation={[-0.01, -0.2, -0.1]}
-			/>
+			{isMobile ? (
+				<primitive
+					object={computer.scene}
+					scale={0.7}
+					position={[0, -3, -2.2]}
+					rotation={[-0.01, -0.2, -0.1]}
+				/>
+			) : (
+				<primitive
+					object={computer.scene}
+					scale={0.75}
+					position={[0, -3.25, -1.5]}
+					rotation={[-0.01, -0.2, -0.1]}
+				/>
+			)}
 		</mesh>
 	);
 };
