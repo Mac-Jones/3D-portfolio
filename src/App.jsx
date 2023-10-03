f9bf1ef88f21a4ee8f62a4aba1115f23f2e8bf69
macjonessoriano <sorianomacjones@gmail.com>
2023-10-03 23:04:16 +0800

    modify styles

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,8 @@ const App = () => {
 	return (
 		<BrowserRouter>
 			<div className='relative z-0 bg-primary'>
-				<div className='relative bg-hero-pattern bg-cover bg-no-repeat bg-center'>
-					<Navbar />
+				<Navbar />
+				<div className='relative bg-hero-pattern bg-cover bg-no-repeat bg-center z-10'>
 					<Hero />
 				</div>
 				<About />
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ const ComputerCanvas = lazy(() => import('./canvas/ComputerCanvas'));
 
 const Hero = () => {
 	return (
-		<section className='relative w-full h-screen flex justify-center items-center'>
+		<section className='relative w-full h-screen flex justify-center items-center '>
 			<div className=' padding-x absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5'>
 				<div className='flex flex-col justify-center items-center mt-5'>
 					<div className='w-5 h-5 rounded-full bg-[#915eff]' />
@@ -24,7 +24,7 @@ const Hero = () => {
 					</p>
 				</div>
 			</div>
-			<ComputerCanvas />
+			<ComputerCanvas className='z-10' />
 			<div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center z-50'>
 				<a href='#about'>
 					<div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
